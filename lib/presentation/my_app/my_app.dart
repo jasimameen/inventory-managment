1bcf08791808e73263a1408c71f4172ebf6839f7
Jasim Ameen <89957373+jasimameenek@users.noreply.github.com>
2022-06-21 13:09:57 +0530

    removed actions reset repo

--- lib/presentation/my_app/my_app.dart
+++ lib/presentation/my_app/my_app.dart
@@ -6,18 +6,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title:const Text('Invendory Managment App'),
+          title: const Text('Invendory Managment App'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+        body: Somethingwidgets(),
       ),
+    );
+  }
+}
+
+class Somethingwidgets extends StatelessWidget {
+  const Somethingwidgets({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello World'),
     );
   }
 }
